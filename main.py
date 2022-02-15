558465c7fe9f189492be3273ba215bc47b922b7a
Zayatsoff <rozin.lior@gmail.com>
2022-02-14 19:23:30 -0500

    Added annotations to main.py

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import streamlit as st
 from webapp import web_app
 from api.gpt import GPT
 
+# pick engine
 GPT_ENGINE = "text-davinci-001"
 
+# assign webapp
 webapp = web_app(engine=GPT_ENGINE)
+
+# create token and temperatre box
 number_tokens = web_app.token_box()
 temperature = web_app.temp_box()
+
+# assign gpt
 gpt = GPT(engine=GPT_ENGINE, temperature=temperature, max_tokens=number_tokens)
+
+# run
 webapp.run(gpt)
